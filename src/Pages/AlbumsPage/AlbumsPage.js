94c988e8c157f369c1f89e3520e0e1a48966ec63
Daniel-W-230 <dwat005@student.monash.edu>
2022-08-06 17:17:09 +1000

    Album page centered and login moved

--- src/Pages/AlbumsPage/AlbumsPage.js
+++ src/Pages/AlbumsPage/AlbumsPage.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import LoginButton from "../../Components/LoginButton";
 import { useAuth0 } from "@auth0/auth0-react";
-import LogoutButton from "../../Components/LogoutButton";
 import AddAlbum from "../../Components/AddAlbum/AddAlbum";
 import AlbumReviewService from "../../Services/album-review.service";
 import "./AlbumsPage.css";
@@ -13,7 +11,6 @@ function AlbumsPage() {
   const [albums, setAlbums] = useState([]);
   const [loading, setLoading] = useState(true);
   const { isAuthenticated, user } = useAuth0();
-  const AuthButton = isAuthenticated ? LogoutButton : LoginButton;
   const role = getRole(user);
   /* Retrieve list of album reviews from database */
 
@@ -35,7 +32,6 @@ function AlbumsPage() {
   } else {
     return (
       <div className="albums-page-container">
-        <AuthButton />
         <Link to="/white-hot-capsicum/">Click to go home</Link>
         <h1>Albums</h1>
         <div className="albums-container">
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import LoginButton from "../../Components/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../../Components/LogoutButton";
 
 function HomePage() {
+  const { isAuthenticated, user } = useAuth0();
+  const AuthButton = isAuthenticated ? LogoutButton : LoginButton;
   return (
     <div className="home-page-container">
       <h1>Home</h1>
       <Link to="/white-hot-capsicum/albums">albums</Link>
       <Link to="/white-hot-capsicum/artists">artists</Link>
+      <AuthButton />
     </div>
   );
 }
